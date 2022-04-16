3722535b681ad5ba373e79e1967d526233b8672c
loxygenk <me@loxygen.dev>
2022-04-16 14:28:05 +0900

    fix: remove temporaly falling test

--- cypress/integration/toppage.spec.ts
+++ cypress/integration/toppage.spec.ts
@@ -7,9 +7,4 @@ describe("Top page", () => {
   it("renders correctly", () => {
     cy.contains("Welcome to Next.js!").should("exist");
   });
-
-  // FIXME: remove unneccesary test
-  it("fails successfully", () => {
-    cy.contains("Welcome to Next.js!").should("not.exist");
-  });
 });
