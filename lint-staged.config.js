6e66207cb732c392a9e3ed981472afc4212e7cc8
loxygenk <me@loxygen.dev>
2022-04-16 10:21:52 +0900

    feat: set up `lint-staged` and `husky`

---
+++ lint-staged.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+function nextLintCommand(filenames) {
+  const optionFileList = "--file " + filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ");
+
+  return `next lint --fix ${optionFileList}`
+}
+
+module.exports = {
+  "./src/**/*.{ts,tsx}": [
+    nextLintCommand,
+    "prettier -cw"
+  ]
+}
