3e8396e20ed069516433d1d40c0b1ed193d663e3
loxygenk <me@loxygen.dev>
2022-04-17 14:40:44 +0900

    feat: create APIClient builder for mocker

---
+++ src/api/interface/buildClient.ts
@@ -0,0 +1,10 @@
+import { APIClient } from "../hook/type";
+import { MockPopulationChangeAPI } from "../mock/population";
+import { MockPrefectureAPI } from "../mock/pref";
+
+export function buildMockClient(): APIClient {
+  return {
+    pref: new MockPrefectureAPI(),
+    population: new MockPopulationChangeAPI(),
+  };
+}
