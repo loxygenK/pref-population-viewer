8c74e04e4b1cb4c9d84af4b494b5c5a408fe613e
loxygenK <me@loxygen.dev>
2022-04-18 11:15:02 +0900

    feat: remove current page content + move Head to _app.ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Header } from "~/components/header";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>pref-population-viewer</title>
+        <meta
+          name="description"
+          content="Visualize the history of the selected prefectures' populationo"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Header />
       <main>
         <Component {...pageProps} />
