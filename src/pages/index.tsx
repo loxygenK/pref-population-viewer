ff909235928aefff7b1913a075c94beae189d998
loxygenK <me@loxygen.dev>
2022-04-18 13:50:18 +0900

    feat: fetch data in the server side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import React from "react";
-import { useAPIClient } from "~/api/hook/context";
-import { APIClient } from "~/api/hook/type";
+import { apiClient } from "~/api/client";
 import { CheckBoxList } from "~/components/checkboxList";
 import { PopulationChangeGraph } from "~/components/graph";
 import { PopulationChange } from "~/domain/polulationChange";
@@ -9,45 +8,12 @@ import { Prefecture } from "~/domain/prefecture";
 import styles from "~/styles/Home.module.scss";
 import { ValueWithID } from "~/types/valueWithId";
 
-const usePrefectures = (api: APIClient): Array<Prefecture> | undefined => {
-  const [pref, setPref] = React.useState<Array<Prefecture> | undefined>();
-
-  React.useEffect(() => {
-    api.pref.fetchPrefectures().then((p) => setPref(p));
-  }, [api]);
-
-  return pref;
-};
-
-const usePopulationChange = (
-  api: APIClient,
-  prefIDs: Array<string>
-): Array<PopulationChange> | undefined => {
-  const [populationChange, setPopulationChange] = React.useState<
-    Array<PopulationChange> | undefined
-  >();
-
-  React.useEffect(() => {
-    api.pref
-      .fetchPrefectures()
-      .then((prefs) => api.population.fetchPopulationChange(prefs))
-      .then((changes) => setPopulationChange(changes));
-  }, [api]);
-
-  const filteredChages = React.useMemo(() => {
-    return populationChange?.filter((c) => prefIDs.includes(c.pref.id));
-  }, [populationChange, prefIDs]);
-
-  return filteredChages;
-};
-
-const Home: NextPage = () => {
+interface HomeProps {
+  prefectures: Array<Prefecture>;
+  populationChanges: Array<PopulationChange>;
+}
+const Home: NextPage<HomeProps> = ({ prefectures, populationChanges }) => {
   const [enabledPrefIDs, setEnabledPrefIDs] = React.useState<string[]>([]);
-
-  // FIXME: Fetch in the server side
-  const api = useAPIClient();
-  const prefecture = usePrefectures(api);
-  const populationChange = usePopulationChange(api, enabledPrefIDs);
 
   const onCheckboxChange = (newCheckedIDs: string[]) => {
     setEnabledPrefIDs(() => newCheckedIDs);
@@ -55,27 +21,38 @@ const Home: NextPage = () => {
 
   const checkboxValues: Array<ValueWithID<string>> | undefined =
     React.useMemo(() => {
-      return prefecture?.map((p) => ({ id: p.id, value: p.name }));
-    }, [prefecture]);
+      return prefectures?.map((p) => ({ id: p.id, value: p.name }));
+    }, [prefectures]);
+
+  const shownPopulationChanges: Array<PopulationChange> = React.useMemo(() => {
+    return populationChanges.filter((p) => enabledPrefIDs.includes(p.pref.id));
+  }, [enabledPrefIDs, populationChanges]);
 
   return (
     <>
-      {checkboxValues !== undefined ? (
-        <CheckBoxList
-          values={checkboxValues}
-          checkedIDs={enabledPrefIDs}
-          onChange={onCheckboxChange}
-        />
-      ) : (
-        <div>Loading...</div>
-      )}
-      {populationChange !== undefined ? (
-        <PopulationChangeGraph populationChanges={populationChange} />
-      ) : (
-        <div>Loading...</div>
-      )}
+      <CheckBoxList
+        values={checkboxValues}
+        checkedIDs={enabledPrefIDs}
+        onChange={onCheckboxChange}
+      />
+      <PopulationChangeGraph populationChanges={shownPopulationChanges} />
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const prefectures = await apiClient.pref.fetchPrefectures();
+  const populationChanges = await apiClient.population.fetchPopulationChange(
+    prefectures
+  );
+
+  return {
+    props: {
+      prefectures,
+      populationChanges,
+    },
+    revalidate: 60 * 60 * 24 * 30,
+  };
 };
 
 export default Home;
