778e25faab1e39975026e8882c074b870853d8a0
loxygenK <me@loxygen.dev>
2022-04-18 13:58:37 +0900

    feat: update test for the index page and its snapshot

--- src/pages/__test__/index.spec.tsx
+++ src/pages/__test__/index.spec.tsx
@@ -1,9 +1,29 @@
 import renderer from "react-test-renderer";
+import { PopulationChange } from "~/domain/polulationChange";
 import Home from "~/pages/index";
 
 describe("index page", () => {
   test("renders correctly", () => {
-    const tree = renderer.create(<Home />).toJSON();
+    const prefectures = Array.from({ length: 5 }).map((_, i) => ({
+      id: `pref-${i}`,
+      name: `Pref #${i}`,
+    }));
+
+    const populationChanges: Array<PopulationChange> = prefectures.map((p) => ({
+      pref: p,
+      forecastBoundary: 2010,
+      changes: [
+        { year: 2000, population: 100000 },
+        { year: 2010, population: 200000 },
+        { year: 2020, population: 300000 },
+      ],
+    }));
+
+    const tree = renderer
+      .create(
+        <Home prefectures={prefectures} populationChanges={populationChanges} />
+      )
+      .toJSON();
 
     expect(tree).toMatchSnapshot();
   });
