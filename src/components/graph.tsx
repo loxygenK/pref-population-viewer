46dd4d0e0a11c4209fb3adde9d84c7cf4bb68c3f
loxygenK <me@loxygen.dev>
2022-04-20 14:50:35 +0900

    feat: split the builder of the graph configuration

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -1,5 +1,5 @@
 import "chart.js/auto";
-import { ChartData, ChartOptions } from "chart.js/auto";
+import { ChartOptions } from "chart.js/auto";
 import { Chart } from "react-chartjs-2";
 import { generateColor } from "~/util/generateColor";
 import { UnitShowerPlugin } from "./graph/unitShowerPlugin";
@@ -17,24 +17,46 @@ export interface PopulationChangeGraphProps {
 export const PopulationChangeGraph: React.FC<PopulationChangeGraphProps> = ({
   populationChanges,
 }) => {
-  // TODO: Show guidance when no changes is shown
-  const data: ChartData<"line"> = React.useMemo(
-    () => ({
-      labels:
-        populationChanges[0] !== undefined
-          ? getActualMeasuredChanges(populationChanges[0]).map((c) => c.year)
-          : [],
-      datasets: populationChanges.map((c, i) => ({
-        label: c.pref.name,
-        backgroundColor: generateColor(i),
-        borderColor: generateColor(i),
-        data: getActualMeasuredChanges(populationChanges[i]).map(
-          (c) => c.population
-        ),
-      })),
-    }),
+  const config = React.useMemo(
+    () => buildGraphConfiguration(populationChanges),
     [populationChanges]
   );
+  if (config === undefined) {
+    return <div></div>;
+  }
+
+  return (
+    <Chart
+      className={styles.graph}
+      type="line"
+      data={config.data}
+      options={config.options}
+      plugins={[
+        new UnitShowerPlugin("x", "年度"),
+        new UnitShowerPlugin("y", "人口数"),
+      ]}
+    />
+  );
+};
+
+const buildGraphConfiguration = (
+  populationChanges: Array<PopulationChange>
+) => {
+  if (populationChanges.length === 0) {
+    return undefined;
+  }
+
+  const data = {
+    labels: getActualMeasuredChanges(populationChanges[0]).map((c) => c.year),
+    datasets: populationChanges.map((c, i) => ({
+      label: c.pref.name,
+      backgroundColor: generateColor(i),
+      borderColor: generateColor(i),
+      data: getActualMeasuredChanges(populationChanges[i]).map(
+        (c) => c.population
+      ),
+    })),
+  };
 
   const options: ChartOptions<"line"> = {
     maintainAspectRatio: false,
@@ -55,17 +77,5 @@ export const PopulationChangeGraph: React.FC<PopulationChangeGraphProps> = ({
     },
   };
 
-  return (
-    <div className={styles.graph}>
-      <Chart
-        type="line"
-        data={data}
-        options={options}
-        plugins={[
-          new UnitShowerPlugin("x", "年度"),
-          new UnitShowerPlugin("y", "人口数"),
-        ]}
-      />
-    </div>
-  );
+  return { data, options };
 };
