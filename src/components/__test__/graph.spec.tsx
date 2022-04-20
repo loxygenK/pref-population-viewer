a608e65447d226f21619428d689f2956d96a6abd
loxygenK <me@loxygen.dev>
2022-04-20 15:10:07 +0900

    feat: create snapshot tests for the both of nothing or something is supplied

--- src/components/__test__/graph.spec.tsx
+++ src/components/__test__/graph.spec.tsx
@@ -2,11 +2,34 @@ import renderer from "react-test-renderer";
 import { PopulationChangeGraph } from "../graph";
 
 describe("Header component", () => {
-  it("renders correctly", () => {
-    // Using empty array for the props `populationChanges` because the snapshot of inside of canvas
-    // (which the graph rendered) cannot be taken.
+  it("renders the suggestion correctly when no populationChange is supplied", () => {
     const tree = renderer
       .create(<PopulationChangeGraph populationChanges={[]} />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders the suggestion correctly when no populationChange is supplied", () => {
+    const tree = renderer
+      .create(
+        <PopulationChangeGraph
+          populationChanges={[
+            {
+              dataIndex: 0,
+              populationChange: {
+                forecastBoundary: 2010,
+                pref: { id: "0", name: "Prefecture" },
+                changes: [
+                  { year: 2000, population: 100000 },
+                  { year: 2010, population: 200000 },
+                  { year: 2020, population: 300000 },
+                ],
+              },
+            },
+          ]}
+        />
+      )
       .toJSON();
 
     expect(tree).toMatchSnapshot();
