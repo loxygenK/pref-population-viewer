393238190c13df7e648a67e55086c745a5fed895
loxygenK <me@loxygen.dev>
2022-04-20 12:31:05 +0900

    refactor: return undefined when the parsing failed

--- src/util/safelyParseNumber.ts
+++ src/util/safelyParseNumber.ts
@@ -1,16 +1,15 @@
 const NUMERIC_REGEX = /-?\d+/;
-export const safelyParseNumber = (original: unknown): number => {
+export const safelyParseNumber = (original: unknown): number | undefined => {
   if (typeof original === "number") {
     return original;
   }
 
   if (typeof original !== "string") {
-    // FIXME: This is temporal, wrap the value by the Result monad thing
-    return NaN;
+    return undefined;
   }
 
   if (!NUMERIC_REGEX.test(original)) {
-    return NaN;
+    return undefined;
   }
 
   return parseFloat(original);
