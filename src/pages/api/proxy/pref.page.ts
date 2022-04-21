bf3361ff5e19383f46b573efa22e0bfa9be74336
Flisan <me@loxygen.dev>
2022-04-21 11:27:30 +0900

    feat: implement the proxy for the prefecture page (#52)
    * feat: implement the proxy for the prefecture page
    
    * feat: write a test for the proxy server

---
+++ src/pages/api/proxy/pref.page.ts
@@ -0,0 +1,19 @@
+import { NextApiHandler } from "next";
+import { apiClient } from "~/api/client";
+import { Prefecture } from "~/domain/prefecture";
+import { ProxyAPIResponse } from "~/dto/proxyAPIResponse";
+
+const handler: NextApiHandler<ProxyAPIResponse<Array<Prefecture>>> = async (
+  _,
+  res
+) => {
+  try {
+    const prefs = await apiClient.pref.fetchPrefectures();
+    res.status(200).json({ status: "success", data: prefs });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ status: "internal_error" });
+  }
+};
+
+export default handler;
