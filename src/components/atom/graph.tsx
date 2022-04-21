4dc9de3205a44a8180cb87eb1b5b8adeed2cba3e
loxygenk <me@loxygen.dev>
2022-04-21 18:05:55 +0900

    feat: make current graph not domain specific

--- src/components/atom/graph.tsx
+++ src/components/atom/graph.tsx
@@ -4,26 +4,25 @@ import { Chart } from "react-chartjs-2";
 import { generateColor } from "~/util/generateColor";
 import { UnitShowerPlugin } from "./graph/unitShowerPlugin";
 
-import styles from "./graph.module.scss";
-import {
-  getActualMeasuredChanges,
-  PopulationChange,
-} from "~/domain/polulationChange";
 import React from "react";
 
-export interface PopulationChangeWithIndex {
-  dataIndex: number;
-  populationChange: PopulationChange;
+export interface GraphData {
+  x: number;
+  y: number;
 }
-export interface PopulationChangeGraphProps {
-  populationChanges: Array<PopulationChangeWithIndex>;
+export interface GraphDataSeries {
+  name: string;
+  data: Array<GraphData>;
 }
-export const PopulationChangeGraph: React.FC<PopulationChangeGraphProps> = ({
-  populationChanges,
-}) => {
+export interface GraphProps {
+  dataSeries: Array<GraphDataSeries>;
+  xUnit: string;
+  yUnit: string;
+}
+export const Graph: React.FC<GraphProps> = ({ dataSeries, xUnit, yUnit }) => {
   const config = React.useMemo(
-    () => buildGraphConfiguration(populationChanges),
-    [populationChanges]
+    () => buildGraphConfiguration(dataSeries),
+    [dataSeries]
   );
   if (config === undefined) {
     return (
@@ -36,38 +35,30 @@ export const PopulationChangeGraph: React.FC<PopulationChangeGraphProps> = ({
   }
 
   return (
-    <div className={styles.graph_area}>
-      <Chart
-        type="line"
-        data={config.data}
-        options={config.options}
-        plugins={[
-          new UnitShowerPlugin("x", "年度"),
-          new UnitShowerPlugin("y", "人口数"),
-        ]}
-      />
-    </div>
+    <Chart
+      type="line"
+      data={config.data}
+      options={config.options}
+      plugins={[
+        new UnitShowerPlugin("x", xUnit),
+        new UnitShowerPlugin("y", yUnit),
+      ]}
+    />
   );
 };
 
-const buildGraphConfiguration = (
-  populationChanges: Array<PopulationChangeWithIndex>
-) => {
-  if (populationChanges.length === 0) {
+const buildGraphConfiguration = (dataSeries: Array<GraphDataSeries>) => {
+  if (dataSeries.length === 0) {
     return undefined;
   }
 
   const data = {
-    labels: getActualMeasuredChanges(populationChanges[0].populationChange).map(
-      (c) => c.year
-    ),
-    datasets: populationChanges.map((c) => ({
-      label: c.populationChange.pref.name,
-      backgroundColor: generateColor(c.dataIndex),
-      borderColor: generateColor(c.dataIndex),
-      data: getActualMeasuredChanges(c.populationChange).map(
-        (c) => c.population
-      ),
+    labels: dataSeries[0].data.map((d) => d.x),
+    datasets: dataSeries.map((s) => ({
+      label: s.name,
+      backgroundColor: generateColor(s.dataIndex),
+      borderColor: generateColor(s.dataIndex),
+      data: s.data.map((d) => d.y),
     })),
   };
 
