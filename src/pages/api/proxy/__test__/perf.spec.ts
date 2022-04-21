a35c92a3258ec0385941f82d6e11b5db6c91c764
loxygenk <me@loxygen.dev>
2022-04-21 11:25:45 +0900

    feat: write a test for the proxy server

---
+++ src/pages/api/proxy/__test__/perf.spec.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import httpMocks from "node-mocks-http";
+import { json } from "stream/consumers";
+import { Prefecture } from "~/domain/prefecture";
+import { ProxyAPIResponse } from "~/dto/proxyAPIResponse";
+import handler from "../pref.page";
+
+describe("Prefecture proxy page", () => {
+  it("fetches the prefecture data", async () => {
+    const req = httpMocks.createRequest<NextApiRequest>();
+    const res = httpMocks.createResponse<NextApiResponse>();
+
+    await handler(req, res);
+
+    const jsonResponse: ProxyAPIResponse<Array<Prefecture>> =
+      res._getJSONData();
+
+    expect(res.statusCode).toBe(200);
+    expect(jsonResponse.status).toBe("success");
+    if (jsonResponse.status !== "success") {
+      return;
+    }
+
+    expect(jsonResponse.data).toHaveLength(50);
+  });
+});
