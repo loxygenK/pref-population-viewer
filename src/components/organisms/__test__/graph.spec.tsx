32d838970803ef244a5e517898cd505b5b5a0924
loxygenk <me@loxygen.dev>
2022-04-21 22:54:50 +0900

    chore: move mocking to set up file

--- src/components/organisms/__test__/graph.spec.tsx
+++ src/components/organisms/__test__/graph.spec.tsx
@@ -1,15 +1,6 @@
 import { PopulationChangeGraph } from "../populationChangeGraph";
 import { renderStatefulComponent } from "~/test/renderStatefulComponent";
 
-import "jest-canvas-mock";
-global.ResizeObserver = class MockResizeObserver {
-  constructor(_callback: ResizeObserverCallback) {}
-
-  disconnect() {}
-  observe(_target: Element, _options?: ResizeObserverOptions) {}
-  unobserve(_target: Element) {}
-};
-
 describe("Header component", () => {
   it("renders the suggestion correctly when no populationChange is supplied", async () => {
     const rendered = await renderStatefulComponent((root) => {
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,17 @@
 import dotenv from "dotenv";
 import path from "path";
 
+import "jest-canvas-mock";
+
+class MockResizeObserver {
+  constructor(_callback: ResizeObserverCallback) {}
+
+  disconnect() {}
+  observe(_target: Element, _options?: ResizeObserverOptions) {}
+  unobserve(_target: Element) {}
+}
+global.ResizeObserver = MockResizeObserver;
+
 export default (): void => {
   dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
 };
