58cb8972806e1647197b5357672513bc17eb9498
loxygenk <me@loxygen.dev>
2022-04-21 20:24:01 +0900

    feat: support showing error

--- src/components/templates/asyncDataHandler.tsx
+++ src/components/templates/asyncDataHandler.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import Skeleton, { SkeletonProps } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
+import styles from "./asyncDataHandler.module.scss";
+
 export interface AsyncDataHandlerProps<T> {
   data: T | undefined;
-  error: any;
+  error: unknown | undefined;
   children: (settledData: T) => React.ReactElement;
   skeletonProps?: SkeletonProps;
 }
@@ -14,5 +16,36 @@ export const AsyncDataHandler = <T,>({
   children,
   skeletonProps,
 }: AsyncDataHandlerProps<T>): React.ReactElement => {
+  if (error !== undefined) {
+    const description = tryStringify(error) ?? "不明なエラーです";
+
+    return (
+      <div>
+        <h3 className={styles.error_heading}>エラーが発生しました</h3>
+        <p>{description}</p>
+      </div>
+    );
+  }
+
   return data !== undefined ? children(data) : <Skeleton {...skeletonProps} />;
+};
+
+const tryStringify = (value: unknown): string | undefined => {
+  if (value == null) {
+    return undefined;
+  }
+
+  if (typeof value !== "object") {
+    return String(value);
+  }
+
+  if (value instanceof Error) {
+    return value.message;
+  }
+
+  if (Object.keys(value).includes("toString")) {
+    return value.toString();
+  }
+
+  return undefined;
 };
