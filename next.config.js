1b4bb532bfd7cba506101a24e0a847708e8f9bc8
loxygenk <me@loxygen.dev>
2022-04-21 11:15:23 +0900

    feat: allow non-tsx file as the page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ["page.tsx"]
+  pageExtensions: ["page.ts", "page.tsx"]
 }
 
 module.exports = nextConfig
